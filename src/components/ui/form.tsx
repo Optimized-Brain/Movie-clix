b5c28fa8d7f7d4e21c79d2e5597b6dc80fd97e3a
Shoaib Attar <143705860+Optimized-Brain@users.noreply.github.com>
2025-06-07 22:05:14 +0530

    Update form.tsx

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -11,7 +11,7 @@ import {
   type FieldPath,
   type FieldValues,
 } from "react-hook-form"
-
+//
 import { cn } from "@/lib/utils"
 import { Label } from "@/components/ui/label"
 
@@ -124,7 +124,7 @@ const FormControl = React.forwardRef<
   )
 })
 FormControl.displayName = "FormControl"
-
+    
 const FormDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
@@ -165,7 +165,7 @@ const FormMessage = React.forwardRef<
   )
 })
 FormMessage.displayName = "FormMessage"
-
+    
 export {
   useFormField,
   Form,
